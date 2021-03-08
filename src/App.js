db2d058780af918e258a2060fc8528d0a32caf6d
Sjoerd Vink <sjoerdvink@icloud.com>
2021-03-08 16:20:01 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import Home from "./Home";
+import Header from "./Header";
+
+const tijd = "00.30.00";
 
 function App() {
   return (
     <div className='app'>
-      <h1>Hello</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import { ExpandMore, Language, SearchOutlined } from "@material-ui/icons";
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <img
+        className='header__icon'
+        src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c513.png'
+        alt='AirBNB Logo'
+      />
+      <div className='header__center'>
+        <input type='text' />
+        <SearchOutlined />
+      </div>
+      <div className='header__right'>
+        <p>Become a host</p>
+        <Language />
+        <ExpandMore />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
