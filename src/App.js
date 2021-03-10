ce33ca279da973a369158c119483456840e77c99
Sjoerd Vink <sjoerdvink@icloud.com>
2021-03-10 10:49:06 +0100

    added homepage including footer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
 
-const tijd = "01.38.00";
+const tijd = "02.05.00";
 
 function App() {
   return (
     <div className='app'>
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
+}
